cc5d142bde8c45792cf7f7331a7bc8de6c1f7e62
Иван Тимошенко <vanya.timoschenko85@gmail.com>
2020-01-12 02:40:31 +0200

    lend

---
+++ build/static/js/form.js
@@ -0,0 +1,8 @@
+(function(w,d,u,b){w['Bitrix24FormObject']=b;w[b] = w[b] || function(){arguments[0].ref=u;
+        (w[b].forms=w[b].forms||[]).push(arguments[0])};
+        if(w[b]['forms']) return;
+        var s=d.createElement('script');s.async=1;s.src=u+'?'+(1*new Date());
+        var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+})(window,document,'https://digitalone.bitrix24.ua/bitrix/js/crm/form_loader.js','b24form');
+
+b24form({"id":"11","lang":"ua","sec":"58aa41","type":"inline"});
